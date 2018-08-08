a8db38cf9e428c565132019f179898619fd861bf
Ariejan de Vroom <ariejan@devroom.io>
2018-08-08 13:45:33 +0200

    Add ssh module

---
+++ modules/ssh.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+preflight_dir="$(cd "$(dirname "$0")/.." && pwd -P)"
+source "$preflight_dir/modules/functions.bash"
+
+info_echo "Checking for SSH keys; generating a new one if it doesn't exist"
+username="$(whoami)"
+hostname="$(hostname)"
+
+[[ -f ~/.ssh/id_rsa.pub ]] || ssh-keygen -t rsa -b 4096 -C "$username@$hostname"
+[[ -f ~/.ssh/id_rsa.pub ]] && cat ~/.ssh/id_rsa.pub
+
