c56018f7b616972472ab6650e9a4c964511bd53e
Ariejan de Vroom <ariejan@devroom.io>
2018-08-08 13:56:49 +0200

    Improve ssh key generation

--- modules/ssh.bash
+++ modules/ssh.bash
@@ -7,6 +7,6 @@ info_echo "Checking for SSH keys; generating a new one if it doesn't exist"
 username="$(whoami)"
 hostname="$(hostname)"
 
-[[ -f ~/.ssh/id_rsa.pub ]] || ssh-keygen -t rsa -b 4096 -C "$username@$hostname"
+[[ -f ~/.ssh/id_rsa.pub ]] || ssh-keygen -t rsa -b 4096 -C "$username@$hostname" -f ~/.ssh/id_rsa -N "" -q
 [[ -f ~/.ssh/id_rsa.pub ]] && cat ~/.ssh/id_rsa.pub
 
