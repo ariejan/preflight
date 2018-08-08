32fdba7b144f78ee49db86d4e226420cd69cf27e
Ariejan de Vroom <ariejan@devroom.io>
2018-08-08 14:58:14 +0200

    Rework iterm dragula install

--- modules/iterm.bash
+++ modules/iterm.bash
@@ -3,11 +3,13 @@
 preflight_dir="$(cd "$(dirname "$0")/.." && pwd -P)"
 source "$preflight_dir/modules/functions.bash"
 
-if [ ! -f "$preflight_dir/Dracula.itermcolors" ]; then
+dragula_theme="$preflight_dir/Dragula.itermcolors"
+
+if [ ! -f "$dragula_theme" ]; then
     info_echo "Installing iTerm2 Dracula theme"
     cd $preflight_dir
-    curl -Ls https://raw.githubusercontent.com/dracula/iterm/master/Dracula.itermcolors
-    open "$preflight_dir/Dracula.itermcolors" -a iTerm
+    curl -Ls https://raw.githubusercontent.com/dracula/iterm/master/Dracula.itermcolors > "$dragula_theme"
+    open "$dragula_theme" -a iTerm
 else
     info_echo "iTerm2 Dracula theme already installed"
 fi
