c9bdca9c70eb0c4bb4897f345b69e47484553937
Ariejan de Vroom <ariejan@devroom.io>
2018-08-08 13:53:06 +0200

    Setup zsh

---
+++ modules/zsh.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+preflight_dir="$(cd "$(dirname "$0")/.." && pwd -P)"
+source "$preflight_dir/modules/functions.bash"
+
+zsh_path="$(which zsh)"
+
+if grep -Fxq "$zsh_path" /etc/shells; then 
+    info_echo "Zsh is already available in /etc/shells"
+else
+    info_echo "Adding zsh to /etc/shells"
+    sudo echo "$zsh_path" >> /etc/shells
+fi
+
+chsh -s $zsh_path
+
